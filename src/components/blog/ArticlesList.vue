5cfc29e6c84a12498d936b6b006ed9eaf3f03c2d
Yurii Faievskyi <urfaevskii@gmail.com>
2018-06-04 23:39:36 +0300

    add simple blog component

---
+++ src/components/blog/ArticlesList.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <single-article></single-article>
+        <single-article></single-article>
+        <single-article></single-article>
+    </div>
+</template>
+
+<script>
+import Article from './Article.vue';
+
+export default {
+    components: {
+        'single-article': Article
+    }
+}
+</script>
+
+<style lang="sass">
+
+</style>
