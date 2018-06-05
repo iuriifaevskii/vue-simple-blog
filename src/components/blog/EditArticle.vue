14c5518b14f97b3a553e3563f1974ef493f88fd1
Yurii Faievskyi <urfaevskii@gmail.com>
2018-06-05 23:14:21 +0300

    add confirmed and beforeRouteLeave hock

--- src/components/blog/EditArticle.vue
+++ src/components/blog/EditArticle.vue
@@ -1,12 +1,28 @@
 <template>
     <div>
         Edit Article
+        <button class="btn btn-primary" @click="confirmed = true">Confirm</button>
     </div>
 </template>
 
 <script>
 export default {
-    
+    data() {
+        return {
+            confirmed: false
+        }
+    },
+    beforeRouteLeave(to, from, next) {
+        if (this.confirmed) {
+            next();
+        } else {
+            if (confirm('are you sure')) {
+                next();
+            } else {
+                next(false);
+            }
+        }
+    }
 }
 </script>
 
